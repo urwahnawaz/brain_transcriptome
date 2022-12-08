5f5927547fe970c8436f0ae54a259e1d116643e5
unawaz1996 <urwah.nawaz@adelaide.edu.au>
2022-12-08 11:47:14 +1100

    changes:

--- preprocessing/functions.R
+++ preprocessing/functions.R
@@ -23,7 +23,7 @@ clean_and_format = function(dir ,dataset){
     phenotype = list.files(dir, full.names = TRUE, pattern = "\\SubjectPhenotypesDS.txt") # Phenotype level information related to each donor 
     exp = list.files(dir, full.names = TRUE, pattern = "\\gene_tpm.gct.gz") # File used for expression matrix 
     
-    gtex.md = read_tsv(attributes, col_types = c('.default' = 'c')) %>% 
+    md = read_tsv(attributes, col_types = c('.default' = 'c')) %>% 
       filter(SMTS == 'Brain') %>% 
       mutate(SUBJID = sapply(str_split(SAMPID, pattern = "-"), function(x) paste(x[1:2], collapse = '-'))) %>%
       left_join(read_tsv(phenotype, col_types = c('.default' = 'c'))) %>%
@@ -32,12 +32,22 @@ clean_and_format = function(dir ,dataset){
       mutate(AGE = paste(.$AGE, "yrs", sep = "") ) %>% as.data.frame() 
     colnames(gtex.md) = annot$BITColumnName[match(colnames(gtex.md), annot$OriginalMetadataColumnName)]
     
-    gtex.md = gtex.md %>% 
+    md = md %>% 
       mutate(Diagnosis = "Control") %>% 
       mutate(SEX = ifelse(SEX == 1, "M", "F")) %>% 
       mutate(Period = "Postnatal") %>% as.data.frame()
      
-    return(gtex.md)
+    colnames(exp) <- gsub("\\.", "-", colnames(exp)) # Changing expression file names to match metadata SampleIDs
+    exp %<>% column_to_rownames("Name")
+    
+    exp = exp %>% 
+      dplyr::select(contains(md$SampleID))
+    message(paste0("Samples subsetted - Exp matrix contains ", ncol(exp), " samples"))
+    rownames(exp) <- sub("\\.[0-9]*$", "", rownames(exp))
+    
+    md= md[which(md$SampleID %in% colnames(exp)),] 
+    
+    
     
     
   } else if (dataset == "BrainSpan"){
@@ -54,7 +64,7 @@ clean_and_format = function(dir ,dataset){
     colnames(columns.metadata) = annot$BITColumnName[match(colnames(columns.metadata), annot$OriginalMetadataColumnName)]
     
     message("Adding additional metadata information")
-    bspan.md = columns.metadata %>% 
+    md = columns.metadata %>% 
       mutate(Stage = add_feature(.$Age, stages), 
              Regions = add_feature(.$StructureAcronym, regions), 
              AgeIntervals = add_feature(.$Age, age_intervals), 
@@ -67,16 +77,22 @@ clean_and_format = function(dir ,dataset){
       dplyr::select("SampleID", everything())
     
     ## Adding Age Numeric 
-    bspan.md$AgeNumeric[grepl("pcw", bspan.md$Age, ignore.case = TRUE)]<-
-      bspan.md$age[grepl("pcw", bspan.md$Age)] %>%  str_remove("_pcw")%>% 
+    md$AgeNumeric[grepl("pcw", md$Age, ignore.case = TRUE)]<-
+      md$age[grepl("pcw", md$Age)] %>%  str_remove("_pcw")%>% 
       as.numeric() %>% `-` (40) %>% divide_by(52)
-    bspan.md$AgeNumeric[grepl("mos", bspan.md$Age, ignore.case = TRUE)] <- 
-      bspan.md$age[grepl("_mos", bspan.md$Age)] %>%  str_remove("_mos") %>% 
+    md$AgeNumeric[grepl("mos", md$Age, ignore.case = TRUE)] <- 
+      md$age[grepl("_mos", md$Age)] %>%  str_remove("_mos") %>% 
       as.numeric() %>% divide_by(12)
-    bspan.md$AgeNumeric[grepl("yrs", bspan.md$Age, ignore.case = TRUE)] <- 
-      bspan.md$age[grepl("_yrs", bspan.md$Age)] %>%  str_remove("_yrs") %>% 
+    md$AgeNumeric[grepl("yrs", md$Age, ignore.case = TRUE)] <- 
+      md$age[grepl("_yrs", md$Age)] %>%  str_remove("_yrs") %>% 
       as.numeric 
     
+    colnames(exp) <- md$SampleID
+    rownames(exp) <- rows.metadata$ensembl_gene_id
+    exp %<>% rownames_to_column("EnsemblID")
+    
+    
+    md = apply(md,2,as.character)
       
       
     
@@ -121,10 +137,10 @@ clean_and_format = function(dir ,dataset){
     comp <- as.data.frame(pd)
     comp <- comp[,57:64]
     m <- match(rownames(comp), rownames(w)) # they are
-    df<- cbind(w, comp[m,])
+    md<- cbind(w, comp[m,])
     
     # Adding features 
-    df = df %>% mutate(Period = ifelse(.$Age > 0, "Postnatal", "Prenatal"), 
+    md = md %>% mutate(Period = ifelse(.$Age > 0, "Postnatal", "Prenatal"), 
                              Region = gsub("HIPPO", "HIP", .$Region)) %>%
       mutate(Regions = add_feature(.$Region, regions)) %>% 
       mutate(age_interval = as.character(cut(Age, seq(-1, 100, by = 10)))) %>%
@@ -134,19 +150,22 @@ clean_and_format = function(dir ,dataset){
       dplyr::select(-age_interval)
     
     
-    df$AgeInterval[df$Age >= -0.76 & df$Age <= -0.70] = "1-3pcw"
-    df$AgeInterval[df$Age >= -0.701 & df$Age <= -0.62] = "4-7pcw"
-    df$AgeInterval[df$Age >= -0.621 & df$Age <= -0.58] = "8-9pcw"
-    df$AgeInterval[df$Age >= -0.57 & df$Age <= -0.53] = "10-12pcw"
-    df$AgeInterval[df$Age >= -0.52 & df$Age <= -0.48] = "13-15pcw"
-    df$AgeInterval[df$Age >= -0.47 & df$Age <= -0.42] = "16-18pcw"
-    df$AgeInterval[df$Age >= -0.41 & df$Age <= -0.30] = "19-24pcw"
-    df$AgeInterval[df$Age >= -0.29 & df$Age <= -0.038] = "25-38pcw"
-    df$AgeInterval[df$Age >= -0.019 & df$Age < 0] = "39-40pcw"
-    df$AgeInterval[df$Age >= 0 & df$Age <= 0.49] <- "0-5mos"
-    df$AgeInterval[df$Age >= 0.50 & df$Age <= 1.58] <- "6-18mos"
-    df$AgeInterval[df$Age >= 1.5833 & df$Age <= 5.99] <- "19mos-5yrs"
-    df$AgeInterval[df$Age >= 6 & df$Age <= 11.99] <- "6-11yrs"  
+    md$AgeInterval[md$Age >= -0.76 & md$Age <= -0.70] = "1-3pcw"
+    md$AgeInterval[md$Age >= -0.701 & md$Age <= -0.62] = "4-7pcw"
+    md$AgeInterval[md$Age >= -0.621 & md$Age <= -0.58] = "8-9pcw"
+    md$AgeInterval[md$Age >= -0.57 & md$Age <= -0.53] = "10-12pcw"
+    md$AgeInterval[md$Age >= -0.52 & md$Age <= -0.48] = "13-15pcw"
+    md$AgeInterval[md$Age >= -0.47 & md$Age <= -0.42] = "16-18pcw"
+    md$AgeInterval[md$Age >= -0.41 & md$Age <= -0.30] = "19-24pcw"
+    md$AgeInterval[md$Age >= -0.29 & md$Age <= -0.038] = "25-38pcw"
+    md$AgeInterval[md$Age >= -0.019 & md$Age < 0] = "39-40pcw"
+    md$AgeInterval[md$Age >= 0 & md$Age <= 0.49] <- "0-5mos"
+    md$AgeInterval[md$Age >= 0.50 & md$Age <= 1.58] <- "6-18mos"
+    md$AgeInterval[md$Age >= 1.5833 & md$Age <= 5.99] <- "19mos-5yrs"
+    md$AgeInterval[md$Age >= 6 & md$Age <= 11.99] <- "6-11yrs"  
+    
+    exp = rse_gene@assays@.xData$data$rpkm
+    rownames(exp) <- sub("\\.[0-9]*$", "", rownames(exp))
     
   }
     
@@ -157,6 +176,15 @@ clean_and_format = function(dir ,dataset){
     message("Invalid dataset")
   }
   
+  
+  md = apply(md,2,as.character)
+  if(file.exists(outdir)) {
+    message("Results directory exists")} else {
+      message("Creating results directory")
+      dir.create(outdir, showWarnings = FALSE)}  
+  message(paste0("Saving results to ", outdir))
+  write.csv(md, paste0(outdir, "/", dataset, "-metadata.csv"))
+  write.csv(exp, paste0(outdir, "/",dataset, "-exp.csv"))
   
   
 }
